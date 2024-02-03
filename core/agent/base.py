0104eb18c0df02354785bcf4b8d6d6405353a64e
tharun-selvam <tharunselvam22@gmail.com>
2024-02-03 16:11:47 +0530

    debug

--- core/agent/base.py
+++ core/agent/base.py
@@ -305,7 +305,7 @@ class Agent:
     def training_set_construction(self, data_dict):
         assert len(list(data_dict.keys())) == 1
         print(data_dict.keys())
-        assert list(data_dict.keys())[0] == "env"
+        # assert list(data_dict.keys())[0] == "env"
         data_dict = data_dict[list(data_dict.keys())[0]]
         print(data_dict.keys())
         states = data_dict['states']
